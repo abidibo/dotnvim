16f0ea5fc563b03c6e4fcbe8efbebbac5091308f
abidibo <abidibo@gmail.com>
2023-09-18 17:00:13 +0200

    feat: configure python dap

---
+++ lua/plugins/nvim-dap-python.lua
@@ -0,0 +1,8 @@
+return {
+    'mfussenegger/nvim-dap-python',
+    lazy = true,
+    ft = { 'python', 'python.django' },
+    config = function()
+        require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+    end,
+}
