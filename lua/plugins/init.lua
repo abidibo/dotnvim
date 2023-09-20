31dcee123a69a3a38ae9e4fbcd98400527d807a5
abidibo <abidibo@gmail.com>
2023-09-20 14:31:18 +0200

    feat: improvements

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,29 +1,58 @@
 return {
-  {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {},
-      config = function () 
-	vim.cmd[[colorscheme tokyonight]]
-      end
-  },
-  "christoomey/vim-tmux-navigator",
-  "skywind3000/asynctasks.vim",
-  "skywind3000/asyncrun.vim",
-  "GustavoKatel/telescope-asynctasks.nvim",
-  "tpope/vim-repeat",
-  "tyru/open-browser.vim",
-  "terryma/vim-multiple-cursors",
-  "aklt/plantuml-syntax",
-  "weirongxu/plantuml-previewer.vim",
-  "junegunn/gv.vim",
-  "sindrets/diffview.nvim",
-  "ap/vim-css-color",
-  "tpope/vim-commentary",
-  "tpope/vim-surround",
-  "lukas-reineke/indent-blankline.nvim",
-  "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-ui",
-  "kristijanhusak/vim-dadbod-completion",
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     config = function ()
+    --       require("tokyonight").setup({
+    --           sidebars = { "qf", "help", "NvimTree" },
+    --       })
+    -- vim.cmd[[colorscheme tokyonight-night]]
+    --     end
+    -- },
+    {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            require('kanagawa').setup({
+                compile = false, -- enable compiling the colorscheme
+                undercurl = true, -- enable undercurls
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { italic = true },
+                statementStyle = { bold = true },
+                typeStyle = {},
+                transparent = false, -- do not set background color
+                dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true, -- define vim.g.terminal_color_{0,17}
+                colors = { -- add/modify theme and palette colors
+                    palette = {},
+                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+                },
+                overrides = function(colors) -- add/modify highlights
+                    return {}
+                end,
+                background = { -- map the value of 'background' option to a theme
+                    dark = "dragon", -- try "dragon" !
+                    light = "lotus"
+                },
+            })
+            vim.cmd("colorscheme kanagawa")
+        end
+    },
+    "christoomey/vim-tmux-navigator",
+    "skywind3000/asynctasks.vim",
+    "skywind3000/asyncrun.vim",
+    "GustavoKatel/telescope-asynctasks.nvim",
+    "tpope/vim-repeat",
+    "tyru/open-browser.vim",
+    "terryma/vim-multiple-cursors",
+    "aklt/plantuml-syntax",
+    "weirongxu/plantuml-previewer.vim",
+    "junegunn/gv.vim",
+    "sindrets/diffview.nvim",
+    "ap/vim-css-color",
+    "tpope/vim-commentary",
+    "tpope/vim-surround",
+    "lukas-reineke/indent-blankline.nvim",
+    "tpope/vim-dadbod",
+    "kristijanhusak/vim-dadbod-ui",
+    "kristijanhusak/vim-dadbod-completion",
 }
--- lua/plugins/vim-illuminate.lua
+++ lua/plugins/vim-illuminate.lua
@@ -1,6 +1,6 @@
 return {
     'RRethy/vim-illuminate',
     config = function ()
-        vim.cmd("hi IlluminatedWordRead guibg=#666666")
+        vim.cmd("hi IlluminatedWordRead guibg=#444444")
     end
 }
