6e5d6b9ccca24838b59f62c4364312c5c50d2ed3
abidibo <abidibo@gmail.com>
2023-09-27 21:20:37 +0200

    feat: add aerial

--- lua/plugins/tagbar.lua
+++ lua/plugins/tagbar.lua
@@ -1,6 +1,7 @@
 return {
     'preservim/tagbar',
     lazy = true,
+    enabled = false,
     cmd = "TagbarToggle",
     keys = {
         {"<leader>8", "<cmd>TagbarToggle<cr>", { desc='Toggle tagbar'}}
