4772e6aaa2eb58aa63b3d53795dae39cbb2f749c
abidibo <abidibo@gmail.com>
2023-09-27 22:53:01 +0200

    feat: plugin config

--- lua/plugins/mini-animate.lua
+++ lua/plugins/mini-animate.lua
@@ -1,7 +1,14 @@
 return {
     'echasnovski/mini.animate',
+    enabled = false,
     version = false,
     config = function()
-        require('mini.animate').setup()
+        require('mini.animate').setup({
+            -- Cursor path
+            cursor = {
+                -- Whether to enable this animation
+                enable = false,
+            }
+        })
     end
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,5 +1,6 @@
 return {
     "folke/noice.nvim",
+    -- enabled = false,
     event = "VeryLazy",
     opts = {
         lsp = {
@@ -8,6 +9,14 @@ return {
                 ["vim.lsp.util.stylize_markdown"] = true,
                 ["cmp.entry.get_documentation"] = true,
             },
+        },
+        popupmenu = {
+            enabled = true, -- enables the Noice popupmenu UI
+            ---@type 'nui'|'cmp'
+            backend = "nui", -- backend to use to show regular cmdline completions
+            ---@type NoicePopupmenuItemKind|false
+            -- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
+            -- kind_icons = {}, -- set to `false` to disable icons
         },
         routes = {
             {
